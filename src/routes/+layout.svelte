00ed79640ca63eb4acf297615835658c07794cc5
meuzi8122 <kagiakete1234@gmail.com>
2026-06-15 00:21:43 +0900

    titleタグ

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,6 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href={favicon} /><title>一方その頃</title></svelte:head>
 <Navbar />
 {@render children()}
