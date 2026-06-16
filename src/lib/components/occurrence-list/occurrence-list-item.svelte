03a8b25e4660e15b02ae62a3b4e7e1939db54a83
meuzi8122 <kagiakete1234@gmail.com>
2026-06-16 18:01:59 +0900

    min-w

--- src/lib/components/occurrence-list/occurrence-list-item.svelte
+++ src/lib/components/occurrence-list/occurrence-list-item.svelte
@@ -24,7 +24,7 @@
 </script>
 
 <div
-	class={`w-full block p-4 border border-gray-500 rounded text-center cursor-pointer ${occurrenceType === "theme2" ? "bg-blue-500" : "bg-red-600"}`}
+	class={`w-full min-w-[354px] block p-4 border border-gray-500 rounded text-center cursor-pointer ${occurrenceType === "theme2" ? "bg-blue-500" : "bg-red-600"}`}
 	draggable="true"
 	role="listitem"
 	ondragstart={(e) => handleDragStart(e, index, occurrenceType)}
