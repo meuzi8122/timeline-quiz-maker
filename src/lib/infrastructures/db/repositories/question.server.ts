5dc32ac41014137cde49c0bcaa9b0857da94d3b0
meuzi8122 <kagiakete1234@gmail.com>
2026-06-16 16:54:03 +0900

    add update method

--- src/lib/infrastructures/db/repositories/question.server.ts
+++ src/lib/infrastructures/db/repositories/question.server.ts
@@ -8,33 +8,55 @@ import {
 
 export function createQuestionRepository(db: DbClient): QuestionRepository {
 	return {
-		async create({ question, occurrencePairs }) {
-			console.log("Creating question with data:", { question, occurrencePairs });
+		async create({ question }) {
 			try {
 				await db.execute({
-					query: `INSERT INTO ${QUESTION_TABLE_NAME} (id, title, theme1, theme2) VALUES (?, ?, ?, ?)`,
-					values: [question.id, question.title, question.theme1, question.theme2]
+					query: `INSERT INTO ${QUESTION_TABLE_NAME} (id, theme1, theme2, description) VALUES (?, ?, ?, ?)`,
+					values: [question.id, question.theme1, question.theme2, question.description]
 				});
 			} catch (error) {
 				console.error("Failed to create question:", error);
 				throw new Error("Failed to create question");
 			}
+		},
+		async update({ question, occurrencePairs }) {
+			try {
+				await db.execute({
+					query: `UPDATE ${QUESTION_TABLE_NAME} SET theme1 = ?, theme2 = ?, description = ? WHERE id = ?`,
+					values: [question.theme1, question.theme2, question.description, question.id]
+				});
+			} catch (error) {
+				console.error("Failed to update question:", error);
+				throw new Error("Failed to update question");
+			}
 
-			// TODO: values句複数指定なり、トランザクションなりでまとめて実行する
-			for (const occurrencePair of occurrencePairs) {
+			if (occurrencePairs.length > 0) {
 				try {
 					await db.execute({
-						query: `INSERT INTO ${OCCURRENCE_PAIRS_TABLE_NAME} (id, question_id, occurrence1, occurrence2) VALUES (?, ?, ?, ?)`,
-						values: [
-							occurrencePair.id,
-							question.id,
-							occurrencePair.occurrence1,
-							occurrencePair.occurrence2
-						]
+						query: `SELECT COUNT(*) AS count FROM ${OCCURRENCE_PAIRS_TABLE_NAME} WHERE question_id = ?`,
+						values: [question.id]
 					});
 				} catch (error) {
-					console.error("Failed to create occurrence-pair:", error);
-					throw new Error("Failed to create occurrence-pair");
+					console.error("Failed to check existing occurrence-pairs:", error);
+					throw new Error("Failed to check existing occurrence-pairs");
+				}
+
+				// TODO: トランザクションで全件成功 or 1件も登録しないを担保
+				for (const occurrencePair of occurrencePairs) {
+					try {
+						await db.execute({
+							query: `INSERT INTO ${OCCURRENCE_PAIRS_TABLE_NAME} (id, question_id, occurrence1, occurrence2) VALUES (?, ?, ?, ?)`,
+							values: [
+								occurrencePair.id,
+								question.id,
+								occurrencePair.occurrence1,
+								occurrencePair.occurrence2
+							]
+						});
+					} catch (error) {
+						console.error("Failed to update occurrence-pair:", error);
+						throw new Error("Failed to update occurrence-pair");
+					}
 				}
 			}
 		}
